b61e2031d42ca5a0a2cc6f7db539356179a5ad2c
Myles Coleman <mylesjcoleman1011@gmail.com>
2026-03-04 12:58:58 +0000

    feat(core): start implementing the multiplayer movement
    Following lecture video to implement client server communication for simple player movement.
    
    Will repurpose once understood for different input system etc, to better fit game.

--- core/src/main/kotlin/uk/ac/tees/e4109732/mam_dungeon_crawler/Main.kt
+++ core/src/main/kotlin/uk/ac/tees/e4109732/mam_dungeon_crawler/Main.kt
@@ -1,8 +1,19 @@
 package uk.ac.tees.e4109732.mam_dungeon_crawler
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
+import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.utils.viewport.ScreenViewport
+import com.badlogic.gdx.utils.viewport.Viewport
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
@@ -10,25 +21,130 @@ import ktx.assets.disposeSafely
 import ktx.assets.toInternalFile
 import ktx.async.KtxAsync
 import ktx.graphics.use
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+import java.net.InterfaceAddress
+import java.net.Socket
+import java.util.concurrent.ConcurrentLinkedQueue
 
 class Main : KtxGame<KtxScreen>() {
     override fun create() {
         KtxAsync.initiate()
 
-        addScreen(FirstScreen())
-        setScreen<FirstScreen>()
+        addScreen(GameScreen())
+        setScreen<GameScreen>()
     }
 }
 
-class FirstScreen : KtxScreen {
-    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
+data class Message(var id: Int, var posX: Float, var posY: Float)
+
+class Character(
+    private val sprite: Sprite,
+    val speed: Int
+) {
+    var posX: Float = 0f
+        private set
+    var posY: Float = 0f
+        private set
+
+    fun update(message: Message) {
+        posX = message.posX
+        posY = message.posY
+    }
+
+    fun draw(batch: SpriteBatch) {
+        sprite.x = posX - sprite.texture.width / 2
+        sprite.y = posY - sprite.texture.height / 2
+        sprite.draw(batch)
+    }
+}
+
+class GameScreen : KtxScreen {
+    private val playerTexture = Texture("circle.png".toInternalFile(), true).apply {  }
+    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) } // Maybe remove
     private val batch = SpriteBatch()
+    private lateinit var players: List<Character>
+    private val coroutineScope = CoroutineScope(Dispatchers.IO + Job())
+    private val queue = ConcurrentLinkedQueue<Message>()
+    private lateinit var camera: OrthographicCamera
+    private lateinit var viewport: Viewport
+    private var mapWidth: Float = 2400f
+    private var mapHeight: Float = 1080f
+    private var playerID: Int = 0
+    private lateinit var tcpSocket: Socket
+    private val playerColours = ListOf(
+        com.badlogic.gdx.graphics.Color.BROWN,
+        com.badlogic.gdx.graphics.Color.GREEN,
+        com.badlogic.gdx.graphics.Color.BLUE,
+    )
+    private val spawnPoints = listOf(
+        Vector2(700f, 500f),
+        Vector2(700f, 100f),
+        Vector2(100f, 500f),
+    )
+
+    override fun show() {
+        camera = OrthographicCamera()
+        mapWidth = Gdx.graphics.width.toFloat()
+        mapHeight = Gdx.graphics.height.toFloat()
+        camera = OrthographicCamera(mapWidth, mapHeight)
+        camera.setToOrtho(false)
+        viewport = ScreenViewport(camera)
+        viewport.update(mapWidth.toInt(), mapHeight.toInt(), true)
+
+        val datagramSocket = DatagramSocket()
+        datagramSocket.send(DatagramPacket("HELLO".toByteArray(), 5,
+            InetAddress.getByName("localhost"), 4301))
+
+        val buffer = ByteArray(1024)
+        val packet = DatagramPacket(buffer, buffer.size)
+        datagramSocket.receive(packet)
+        tcpSocket = Socket(packet.address, 4300)
+        val inputStream = tcpSocket.getInputStream()
+        var bytes = ByteArray(4)
+        inputStream.read(bytes)
+        playerID = bytes[0].toInt()
+        players = List(3) { i ->
+            val sprite = Sprite(playerTexture)
+            sprite.color = playerColours[i]
+            val character = Character(sprite, 300)
+            character.update(Message(i, spawnPoints[i].x, spawnPoints[i].y))
+            character
+        }
+
+        coroutineScope.launch {
+            while (true) {
+                bytes = ByteArray(1024)
+                val length = inputStream.read(bytes)
+                if (length > 0) {
+                    val message = String(bytes, 0, length).split(",")
+                    if (message.size == 3) {
+                        val id = message[0].toInt()
+                        val posX = message[1].toFloat()
+                        val posY = message[2].toFloat()
+                        queue.add(Message(id, posX, posY))
+                    }
+                }
+            }
+        }
+    }
 
     override fun render(delta: Float) {
-        clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
-        batch.use {
-            it.draw(image, 100f, 160f)
-        }
+        logic(delta)
+        draw(delta)
+    }
+
+    private fun logic(delta: Float) {
+        super.show()
+        val moveSpeed = delta * players[playerID].speed
+        val oldPosition = Vector2(players[playerID].posX, players[playerID].posY)
+        val newPosition = Vector2(players[playerID].posX, players[playerID].posY)
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) newPosition.y += moveSpeed
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) newPosition.x -= moveSpeed
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) newPosition.y -= moveSpeed
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) newPosition.x += moveSpeed
+
     }
 
     override fun dispose() {
