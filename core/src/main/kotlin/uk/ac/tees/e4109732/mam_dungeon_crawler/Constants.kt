74a7c9edf14515c92ba6b51eea34a7a60cd980f3
MylesColeman <mylesjcoleman1011@gmail.com>
2026-03-31 15:00:43 +0100

    chore(mechanic): remove redundant box2d constants
    Removed the collision bits from constant, unused.

--- core/src/main/kotlin/uk/ac/tees/e4109732/mam_dungeon_crawler/Constants.kt
+++ core/src/main/kotlin/uk/ac/tees/e4109732/mam_dungeon_crawler/Constants.kt
@@ -6,9 +6,4 @@ object Constants {
     const val UNIT_SCALE = 1f / 8f
     const val MAP_WIDTH = 20
     const val MAP_HEIGHT = 11
-
-    const val BIT_PLAYER: Short = 0x0001
-    const val BIT_WALL: Short = 0x0002
-    const val BIT_ENEMY: Short = 0x0004
-    const val BIT_COLLECTABLE: Short = 0x0008
 }
