2eadadfee10ad14a821408b2857ac35ea09ea032
MylesColeman <mylesjcoleman1011@gmail.com>
2026-05-11 20:31:03 +0100

    fix(mechanic): update to use correct ip

--- core/src/main/kotlin/uk/ac/tees/e4109732/mam_dungeon_crawler/Constants.kt
+++ core/src/main/kotlin/uk/ac/tees/e4109732/mam_dungeon_crawler/Constants.kt
@@ -1,7 +1,7 @@
 package uk.ac.tees.e4109732.mam_dungeon_crawler
 
 object Constants {
-    const val IP_ADDRESS = "127.0.0.1" // Held in constant for easy changing
+    const val IP_ADDRESS = "192.168.0.25" // Held in constant for easy changing
 
     const val UNIT_SCALE = 1f / 8f // Conversion, map pixels to world units (1 unit = 8px tile)
     // Map dimensions, in tiles
